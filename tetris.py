93a7fdbbbb64afb5e6b4d6d6d4ca0f1ee45e66cf
Kalebe <profissional.kalebe@gmail.com>
2023-02-22 21:05:19 -0300

    Tetris "Final Version"
     Changes to be committed:
    	new file:   Assents/Fonts/Designer.otf
    	renamed:    Images File/Logo.png -> Assents/Logo.png
    	new file:   Assents/Sprites/0.png
    	new file:   Assents/Sprites/1.png
    	new file:   Assents/Sprites/2.png
    	new file:   Assents/Sprites/3.png
    	new file:   Assents/Sprites/4.png
    	new file:   Assents/Sprites/5.png
    	new file:   __pycache__/main.cpython-311.pyc
    	modified:   __pycache__/settings.cpython-311.pyc
    	modified:   __pycache__/tetris.cpython-311.pyc
    	modified:   __pycache__/tetromino.cpython-311.pyc
    	modified:   main.py
    	modified:   settings.py
    	modified:   tetris.py
    	modified:   tetromino.py

--- tetris.py
+++ tetris.py
@@ -1,12 +1,96 @@
 from settings import *
 import math
 from tetromino import Tetromino
+import pygame.freetype as ft
+
+class Text:
+    def __init__(self, app):
+        self.app = app
+        self.font = ft.Font(FONT_PATH)
+
+    def draw(self):
+        self.font.render_to(self.app.screen, (WIN_W * 0.595, WIN_H * 0.02),
+                            text='TETRIX', fgcolor='white',
+                            size=TILE_SIZE * 1.65, bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W * 0.65, WIN_H * 0.22),
+                            text='next', fgcolor='orange',
+                            size=TILE_SIZE * 1.4, bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W * 0.65, WIN_H * 0.67),
+                            text='score', fgcolor='orange',
+                            size=TILE_SIZE * 1.4, bgcolor='black')
+        self.font.render_to(self.app.screen, (WIN_W * 0.65, WIN_H * 0.8),
+                            text=f'{self.app.tetris.score}', fgcolor='white',
+                            size=TILE_SIZE * 1.8)
+
 
 class Tetris:
     def __init__(self, app):
         self.app = app
         self.sprites_group = pygame.sprite.Group()
+        self.field_array = self.get_field_array()
         self.tetromino = Tetromino(self)
+        self.next_tremomino = Tetromino(self, current=False)
+        self.speed_up = False
+
+        self.score = 0
+        self.full_lines = 0
+        self.points_per_lines = {0: 0, 1: 100, 2:300, 3: 700, 4:1500}
+
+    def get_score(self):
+        self.score += self.points_per_lines[self.full_lines]
+        self.full_lines = 0
+
+    def check_full_lines(self):
+        row = FIELD_H - 1
+        for y in range(FIELD_H - 1, -1, -1):
+            for x in range(FIELD_W):
+                self.field_array[row][x] = self.field_array[y][x]
+
+                if self.field_array[y][x]:
+                    self.field_array[row][x].pos = vec(x,y)
+
+            if sum(map(bool, self.field_array[y])) < FIELD_W:
+                row -= 1
+            else:
+                for x in range(FIELD_W):
+                    self.field_array[row][x].alive = False
+                    self.field_array[row][x] = 0
+                
+                self.full_lines += 1
+
+    def put_tetromino_blocks_in_array(self):
+        for block in self.tetromino.blocks:
+            x, y = int(block.pos.x), int(block.pos.y)
+            self.field_array[y][x] = block
+
+    def get_field_array(self):
+        return [[0 for x in range(FIELD_W)] for y in range(FIELD_H)]
+
+    def is_game_over(self):
+        if self.tetromino.blocks[0].pos.y == INIT_POS_OFFSET[1]:
+            pygame.time.wait(300)
+            return True
+
+    def check_tetromino_landing(self):
+        if self.tetromino.landing:
+            if self.is_game_over():
+                self.__init__(self.app)
+            else:
+                self.speed_up = False
+                self.put_tetromino_blocks_in_array()
+                self.next_tremomino.current = True
+                self.tetromino = self.next_tremomino
+                self.next_tremomino = Tetromino(self, current=False)
+
+    def control(self, pressed_key):
+        if pressed_key == pygame.K_LEFT or pressed_key == pygame.K_a:
+            self.tetromino.move(direction='left')
+        elif pressed_key == pygame.K_RIGHT or pressed_key == pygame.K_d:
+            self.tetromino.move(direction='right')
+        elif pressed_key == pygame.K_UP or pressed_key == pygame.K_w:
+                self.tetromino.rotate()
+        elif pressed_key == pygame.K_DOWN or pressed_key == pygame.K_s:
+                self.speed_up = True
 
     def draw_grid(self):
         for x in range(FIELD_W):
@@ -15,7 +99,12 @@ class Tetris:
                 (x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE), 1)
 
     def update(self):
-        self.tetromino.update()
+        trigger = [self.app.anim_trigger, self.app.fast_anim_trigger][self.speed_up]
+        if trigger:
+            self.check_full_lines()
+            self.tetromino.update()
+            self.check_tetromino_landing()
+            self.get_score()
         self.sprites_group.update()
 
     def draw(self):
